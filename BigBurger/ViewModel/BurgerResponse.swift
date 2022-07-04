26251e1e2745aa18746ad5c070dad63d39192afe
Corentin <corentin.medina@cgi.com>
2022-07-04 18:00:47 +0200

    🏗️ Added BurgerResponse model

---
+++ BigBurger/ViewModel/BurgerResponse.swift
@@ -0,0 +1,21 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let burgerResponse = try? newJSONDecoder().decode(BurgerResponse.self, from: jsonData)
+
+import Foundation
+
+// MARK: - BurgerResponseElement
+struct BurgerResponseElement: Codable {
+    let ref, title, burgerResponseDescription: String
+    let thumbnail: String
+    let price: Int
+
+    enum CodingKeys: String, CodingKey {
+        case ref, title
+        case burgerResponseDescription = "description"
+        case thumbnail, price
+    }
+}
+
+typealias BurgerResponse = [BurgerResponseElement]
